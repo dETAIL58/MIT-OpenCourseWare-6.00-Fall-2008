9e6e75fbc9593fa93efb9f822ef7e932c8bad0f2
dETAIL58 <detail58@gmail.com>
2012-09-02 20:20:35 -0700

    updated ps8 #2 with sorted and lambda and then added timers #4 for comparison

--- ps8.py
+++ ps8.py
@@ -9,6 +9,12 @@
 
 import time
 
+# creating a small catalog to test with
+##fo = open('smallCatalog.txt', 'w')
+##fo.write("6.00,16,8\n1.00,7,7\n6.01,5,3\n15.01,9,6")
+##fo.close()
+
+##SUBJECT_FILENAME = "smallCatalog.txt"
 SUBJECT_FILENAME = "subjects.txt"
 VALUE, WORK = 0, 1
 
@@ -102,36 +108,79 @@ def greedyAdvisor(subjects, maxWork, comparator):
     if len(subjects) == 0:
         return 'Empty SubjectList'
     assert(maxWork >= 0), "No hours of work"
+    assert(comparator is cmpValue or comparator is cmpWork or comparator is cmpRatio), "Not a valid comparator." 
     
-    result, resultUpdated = {}, True
+    result = {}
     totalWork = 0
 
-    subNames = subjects.keys()
+    if comparator is cmpValue:
+        sortedSubjects = sorted(subjects.iteritems(), key=lambda (k,v): v[VALUE], reverse=True)
+    elif comparator is cmpWork:
+        sortedSubjects = sorted(subjects.iteritems(), key=lambda (k,v): v[WORK])
+    elif comparator is cmpRatio:
+        sortedSubjects = sorted(subjects.iteritems(), key=lambda (k,v): float(v[VALUE]) / v[WORK], reverse=True)
+    else: print "Not a valid comparator."
 
-    while resultUpdated:
-        resultUpdated = False
-        for x in subNames:
-            greater = False
-            if subjects[x][WORK] + totalWork <= maxWork:
-                for y in subNames:
-                    if result.get(y,'0') == '0' and subjects[y][WORK] + totalWork <= maxWork:
-                        if comparator((subjects[x][VALUE], subjects[x][WORK]), (subjects[y][VALUE], subjects[y][WORK])):
-                            greater = True
-                        elif comparator is cmpValue and subjects[x][VALUE] == subjects[y][VALUE]:
-                            greater = True
-                        elif comparator is cmpWork and subjects[x][WORK] == subjects[y][WORK]:
-                            greater = True
-                        elif comparator is cmpRatio and float(subjects[x][VALUE]) / subjects[x][WORK] == float(subjects[y][VALUE]) / subjects[y][WORK]:
-                            greater = True
-                        elif subjects[x] is not subjects[y]:
-                            greater = False
-                            break
-                if greater:
-                    totalWork += subjects[x][WORK]
-                    result[x] = (subjects[x][VALUE], subjects[x][WORK])
-                    resultUpdated = True
+    for x in range(len(sortedSubjects)):
+        if sortedSubjects[x][1][WORK] + totalWork <= maxWork:
+            totalWork += sortedSubjects[x][1][WORK]
+            result[sortedSubjects[x][0]] = (sortedSubjects[x][1][VALUE], sortedSubjects[x][1][WORK])
+            if totalWork == maxWork: break
     return result
+
+
+##
+##  The old way using the cmpFunctions that came with the problem set,
+##  before I learned the cool sorted and lambda functions.
+##  Uncomment out the below code and comment out the code above to use.
+##
+##    resultUpdated = True
+##    subNames = subjects.keys()
+##
+##    while resultUpdated:
+##        resultUpdated = False
+##        for x in subNames:
+##            greater = False
+##            if subjects[x][WORK] + totalWork <= maxWork:
+##                for y in subNames:
+##                    if result.get(y,'0') == '0' and subjects[y][WORK] + totalWork <= maxWork:
+##                        if comparator((subjects[x][VALUE], subjects[x][WORK]), (subjects[y][VALUE], subjects[y][WORK])):
+##                            greater = True
+##                        elif comparator is cmpValue and subjects[x][VALUE] == subjects[y][VALUE]:
+##                            greater = True
+##                        elif comparator is cmpWork and subjects[x][WORK] == subjects[y][WORK]:
+##                            greater = True
+##                        elif comparator is cmpRatio and float(subjects[x][VALUE]) / subjects[x][WORK] == float(subjects[y][VALUE]) / subjects[y][WORK]:
+##                            greater = True
+##                        elif subjects[x] is not subjects[y]:
+##                            greater = False
+##                            break
+##                if greater:
+##                    totalWork += subjects[x][WORK]
+##                    result[x] = (subjects[x][VALUE], subjects[x][WORK])
+##                    resultUpdated = True
+##    return result
             
+
+def greedyAdvisorTime():
+    """
+    Runs tests on bruteForceAdvisor and measures the time required to compute
+    an answer.
+    """
+    subjects = loadSubjects(SUBJECT_FILENAME)
+    printSubjects(subjects)
+    print "------------------------------\n" * 2
+
+    for maxWork in range(1,100):
+        start = time.time()
+        selected = greedyAdvisor(subjects, maxWork, cmpValue)
+        end = time.time()
+        printSubjects(selected)
+        print "maxWork: %d\tTime Spent: %0.2f" % (maxWork,end-start)
+        print "------------------------------\n"
+        if end - start > 5:
+            print "The last greedy attempt took longer than 5 seconds, aborting..."
+            break
 
     
 def bruteForceAdvisor(subjects, maxWork):
@@ -185,7 +234,20 @@ def bruteForceTime():
     Runs tests on bruteForceAdvisor and measures the time required to compute
     an answer.
     """
-    # TODO...
+    subjects = loadSubjects(SUBJECT_FILENAME)
+    printSubjects(subjects)
+    print "------------------------------\n" * 2
+
+    for maxWork in range(1,11):
+        start = time.time()
+        selected = bruteForceAdvisor(subjects, maxWork)
+        end = time.time()
+        printSubjects(selected)
+        print "maxWork: %d\tTime Spent: %0.2f" % (maxWork,end-start)
+        print "------------------------------\n"
+        if end - start > 5:
+            print "The last bruteForce attempt took longer than 5 seconds, aborting..."
+            break
 
 # Problem 3 Observations
 # ======================
@@ -223,30 +285,3 @@ def dpTime():
 # how its performance compares to that of bruteForceAdvisor.
 
 
-#
-# Test shit
-#
-def test():
-    # creating a small catalog to test with
-    fo = open('smallCatalog.txt', 'w')
-    fo.write("6.00,16,8\n1.00,7,7\n6.01,5,3\n15.01,9,6")
-    fo.close()
-    
-    # create the subject dictionary
-#    subjects = loadSubjects('smallCatalog.txt')
-    subjects = loadSubjects(SUBJECT_FILENAME)
-    
-    # print the dictionary
-    printSubjects(subjects)
-
-##    selected = greedyAdvisor(subjects, 15, cmpValue)
-##    printSubjects(selected)
-    selected = bruteForceAdvisor(subjects, 15)
-    printSubjects(selected)
-    
-#    selected = greedyAdvisor(subjects, 15, cmpWork)
-#    printSubjects(selected)
-#    selected = greedyAdvisor(subjects, 15, cmpRatio)
-#    printSubjects(selected)
-
-test()
